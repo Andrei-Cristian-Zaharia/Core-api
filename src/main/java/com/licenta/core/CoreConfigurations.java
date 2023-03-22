4dbcf06bc4c52134a3b3856d0d78b2c7223cc2a9
Asar <zaharia.andrei2010@gmail.com>
2023-03-22 22:36:25 +0200

    Upload updates 22.03.2023

--- src/main/java/com/licenta/core/CoreConfigurations.java
+++ src/main/java/com/licenta/core/CoreConfigurations.java
@@ -2,12 +2,13 @@ package com.licenta.core;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "core")
 public class CoreConfigurations {
 
